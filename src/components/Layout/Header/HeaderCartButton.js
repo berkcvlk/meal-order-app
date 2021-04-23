b75a6c5a1b1d6b3b5bce23765da86ed7b53c0461
Berk Civelek <berkcvlk@gmail.com>
2021-04-24 01:05:16 +0300

    Cart animation added

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCxt = useContext(CartContext);
 
-  const numberOfCartItems = cartCxt.items.reduce((count, item) => {
+  const { items } = cartCxt;
+
+  const numberOfCartItems = items.reduce((count, item) => {
     return count + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onToggleCart} className={classes.button}>
+    <button onClick={props.onToggleCart} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
